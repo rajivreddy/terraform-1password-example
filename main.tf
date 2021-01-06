3003f1dec8e049684e50897e1ad95566aa6c0a9d
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-01-06 16:16:52 +0530

    adding OP to path

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@
 # }
 resource "null_resource" "install_op" {
 
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     command = "wget https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_amd64_v1.8.0.zip; unzip op_linux_amd64_v1.8.0.zip;mv op /usr/local/bin;echo $PATH;op --version"
   }
