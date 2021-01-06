e6ce3c7df7b557c82c2e7a3ac59f4e2188f40fca
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-01-06 16:00:29 +0530

    Adding 1password provider

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 provider "onepassword" {
-    email      = "rajeev.jaggavarapu@srijan.net"
-    password   = "23wesdxc@#WESDXC$$##%%"
-    secret_key = "A3-ZJ4GK5-3QDRWG-DDX8R-QRBC3-V4YFD-NNH29"
-    subdomain  = "oncorps"
 }
+
+
 provider "aws" {
   region = "us-west-2"
 }
 
 resource "null_resource" "install_op" {
-provisioner "local-exec" {
-    command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
+
+  provisioner "local-exec" {
+    command = "wget https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_amd64_v1.8.0.zip; unzip op_linux_amd64_v1.8.0.zip;echo $PATH "
   }
 }
