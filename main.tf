a58c19a0821469ebfd11157b2c379b8a5ee05d0b
Rajiv Reddy <rajeev.jaggavarapu@srijan.net>
2021-01-07 12:09:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ resource "null_resource" "install_op" {
   }
 
   provisioner "local-exec" {
-    command = "whoami; wget https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_amd64_v1.8.0.zip; unzip op_linux_amd64_v1.8.0.zip;echo $PATH;export PATH="$PATH:$PWD";echo $PATH; op --version"
+    command = "whoami; wget https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_amd64_v1.8.0.zip; unzip op_linux_amd64_v1.8.0.zip;echo $PATH;export PATH=$PWD:$PATH;echo $PATH; op --version"
   }
 }
